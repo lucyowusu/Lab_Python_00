9a35d29b746f6b6ed1ee106058cef9e63c418070
AITI Student <aiti-student@aiti-088-PC.(none)>
2012-06-19 16:51:14 +0000

    assigment 1

---
+++ Day1.py
@@ -0,0 +1,54 @@
+print'Lucy Owusu'
+print'Python here we come'
+
+
+print'Ex 1.5'
+print'Ex 1'
+a=11
+b=2
+c=a/b
+print 'the output of a/b is',c
+
+
+d=11
+f=2
+g=d/f
+print'g is equal to ', g
+
+
+print'Ex 1.5'
+print'Ex 2'
+
+f=3
+g=27
+q=pow(f,g)
+print 'the output of pow(f,g) is' ,q
+
+print'Ex 1.5'
+print'Ex 3'
+
+h=134533
+t=6889980
+n=84849
+e=a+b+c
+
+print 'the answer=' ,e
+
+
+
+print'Ex 1.5'
+print'Ex 4'
+
+converted = 7.9*1000
+print 'converting 7.9km to meters =' ,converted
+print 'the average time per meter is ', 45/converted
+
+print'Ex 1.5'
+print'Ex 5'
+
+lucy=4.7
+sela=9.4
+
+print 'the shortest distance between you and your friend is ', pow((pow(lucy,2)+pow(sela,2)),0.5)*0.62137
+
+
